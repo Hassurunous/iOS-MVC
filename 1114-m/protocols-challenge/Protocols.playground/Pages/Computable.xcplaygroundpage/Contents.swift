ca2730933c04458dfd0e8319f19466ad84ff1b59
Michael Loubier <michael.a.loubier@gmail.com>
2016-11-16 12:13:45 -0800

    Completed Mood Tracker and Protocol playground.

--- 1114-m/protocols-challenge/Protocols.playground/Pages/Computable.xcplaygroundpage/Contents.swift
+++ 1114-m/protocols-challenge/Protocols.playground/Pages/Computable.xcplaygroundpage/Contents.swift
@@ -6,20 +6,39 @@ protocol Computable {
     func compute() -> Int
 }
 
-class Sum {
-  var numbers = [1,2,3,4,5,6,7,8,9]
+class Sum: Computable {
+    var numbers = [1,2,3,4,5,6,7,8,9]
+    func compute() -> Int {
+        var sum = 0
+        for number in numbers {
+            sum = sum + number
+        }
+        return sum
+    }
 }
 
-class Product {
-  var numbers = [1,2,3,4,5,6,7,8,9]
+class Product: Computable {
+    var numbers = [1,2,3,4,5,6,7,8,9]
+    func compute() -> Int {
+        var product = 1
+        for number in numbers {
+            product = product * number
+        }
+        return product
+    }
 }
 
-class CountCharacters {
-  var string: String = "odinfeoivneflndwcnervbunlinwe;dqwmid"
+class CountCharacters: Computable {
+    var string: String = "odinfeoivneflndwcnervbunlinwe;dqwmid"
+    func compute() -> Int {
+        return string.characters.count
+    }
 }
 
-class Random {
-  
+class Random: Computable{
+    func compute() -> Int {
+        return Int(arc4random())
+    }
 }
 
 let sum = Sum()
@@ -27,10 +46,13 @@ let product = Product()
 let countCharacters = CountCharacters()
 let random = Random()
 
-//let arrayOfComputables = [sum, product, countCharacters, random]
+let arrayOfComputables:[Computable] = [sum, product, countCharacters, random]
+for index in arrayOfComputables {
+    print(index.compute())
+}
 
 /*:
- **Tasks**: 
+ **Tasks**:
  1. Uncomment above line and make the code compile (by implementing the `Computable` protocol in all the classes)
  2. Iterate over `arrayOfComputables`, _compute_ each result and print it to the console using `print`
  */
